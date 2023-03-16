97b0616159b536c7bb369e09f89560ff3a5d1f5e
joseph reis <josephreis646@gmail.com>
2023-03-16 17:56:49 -0400

    Defined columns for Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,6 +1,5 @@
 // Tag.js 
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
@@ -8,6 +7,26 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,

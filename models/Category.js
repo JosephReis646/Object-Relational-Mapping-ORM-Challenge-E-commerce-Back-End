3322689466a87c11eadfb748cefbb1a5c45d6a07
joseph reis <josephreis646@gmail.com>
2023-03-16 17:54:13 -0400

    Defined catagories for Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,5 @@
 // Category.js 
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
@@ -8,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
